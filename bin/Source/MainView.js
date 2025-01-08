f5a424dcebe6718250790b0f06becdbd0cbdf02c
Maeng JuYoung <wndud6302@naver.com>
2025-01-08 16:30:02 +0900

    데이터 그리드에 로드하기

--- bin/Source/MainView.js
+++ bin/Source/MainView.js
@@ -14,6 +14,7 @@ MainView = class MainView extends AView
 		super.init(context, evtListener)
 
 		//TODO:edit here
+        this.data = ''
 
 	}
 
@@ -22,6 +23,10 @@ MainView = class MainView extends AView
 		super.onInitDone()
 
 		//TODO:edit here
+        this.getItemInfo();
+        this.tab.selectTabById('home');
+        
+
 
 	}
 
@@ -32,6 +37,56 @@ MainView = class MainView extends AView
 		//TODO:edit here
 
 	}
+
+    getItemInfo(){
+        const thisObj = this, 
+        serviceKey = 'iLRN%2FNmqT6sKaIKpIX5W2XnVJYAkR2Ygqxhs6ep8RKbiSEa1TLSsmhRhFTp8o3iCCCOvKfJXIva2pRivDOuFuw%3D%3D', // 일반 인증키
+        stdt = '2024',  // 조회시작년도 
+        url = 'https://apis.data.go.kr/1160100/service/GetKrxListedInfoService/getItemInfo?serviceKey=' + serviceKey + '&numOfRows=100&pageNo=1&resultType=json';
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(result){
+                thisObj.data = result.response.body.items.item;
+                console.log("result=",thisObj.data);
+            }
+        })
+    }
+
+
+
+	onTabClick(comp, info, e)
+	{
+        const thisObj = this;
+        const tabId = comp.compId;
+
+        thisObj.tab.selectTabById(tabId);
+        console.log("탭 id = ",tabId);
+
+
+        const tab = this.tab.getSelectedTab();
+        
+
+        if (tabId == 'home'){
+            const grid = tab.view.grid;
+            const items = thisObj.data;
+            console.log("items",items)
+            for(var i = 0; i < items.length; i++){
+                grid.addRow([
+                    items[i].basDt,
+                    items[i].itmsNm,
+                    items[i].mrktCtg,
+                    items[i].isinCd,
+                    items[i].corpNm,
+                    items[i].crno,
+                    items[i].srtnCd
+                ])
+            }
+        }
+
+	}
+
 
 }
 
