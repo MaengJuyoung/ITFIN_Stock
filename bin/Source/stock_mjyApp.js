64d3ed260388337d7b937514fff1cb74871a986e
Maeng JuYoung <wndud6302@naver.com>
2025-01-08 15:03:39 +0900

    layout 초기 셋팅

---
+++ bin/Source/stock_mjyApp.js
@@ -0,0 +1,33 @@
+
+stock_mjyApp = class stock_mjyApp extends AApplication
+{
+	constructor()
+	{
+		super()
+
+		//TODO:edit here
+
+	}
+
+	onReady()
+	{
+		super.onReady();
+
+		this.setMainContainer(new APage('main'))
+		this.mainContainer.open('Source/MainView.lay')
+
+		//TODO:edit here
+
+	}
+
+	unitTest(unitUrl)
+	{
+		//TODO:edit here
+
+		this.onReady()
+
+		super.unitTest(unitUrl)
+	}
+
+}
+
